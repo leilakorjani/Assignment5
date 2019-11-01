eefb4a7cd68fb67652449493e831c874b6f568a9
Leila Korjani <Leila.Korjani@student.csulb.edu>
2019-10-31 17:42:21 -0700

    third commit

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let postEmail = document.getElementById('firstbox');
+let postUser = document.getElementById('secondbox');
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
 // xhr.addEventListener('load');
-// xhr.responseType = 'json';
+xhr.responseType = 'json';
 xhr.onload = () => {
     if(xhr.status === 200){
-        let data = JSON.parse(xhr.response);
-        // let data = xhr.response;
+        // let data = JSON.parse(xhr.response);
+        let data = xhr.response;
         let lenData = data.length;
         let cumEmail= '<ul>';
         for(let i=0; i<lenData ; i++){
@@ -23,3 +24,23 @@ xhr.onload = () => {
     };
 };   
 xhr.send();
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(response =>{
+    return response.json();
+})
+.then(responseData =>{
+    let user = responseData;
+    let lenUser = user.length;
+    let cumUser = '<ul>';
+    for(let i=0; i<lenUser ; i++){
+        user.sort( function( a, b ) {
+            return a.username.length - b.username.length;
+        });
+        cumUser += '<li>'+ user[i].username + '</li>';
+    }
+    console.log(cumUser);
+    cumUser += '</ul>'
+    postUser.innerHTML = cumUser;
+
+});
